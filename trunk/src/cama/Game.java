b6804256c9bbd80ae95d29216f7db1ac90944aa2
chukreev55 <chukreev55@69d88170-274e-2ec1-5a88-701c5e5b7dbd>
2010-10-24 05:19:09 +0000

    Добавили проект в репозиторий

---
+++ trunk/src/cama/Game.java
@@ -0,0 +1,16 @@
+package cama;
+import java.io.*;
+class Game
+{
+    static public void main(String[]args) throws IOException
+    {
+        BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        konsol ks = new konsol();
+        ks.start();        
+        w.write("\r\n"+"Enter your names: ");
+        w.flush();
+        String[]s = r.readLine().split(" ");
+        w.close();
+    }
+}
---
+++ trunk/src/cama/HP.java
@@ -0,0 +1,4 @@
+package cama;
+class HP {
+    
+}
