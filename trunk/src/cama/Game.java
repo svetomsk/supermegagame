7a9bc03762718978175d9805ef0898e6348ea897
www.milazar@mail.ru <www.milazar@mail.ru@69d88170-274e-2ec1-5a88-701c5e5b7dbd>
2010-11-13 16:34:02 +0000

    Зациклил ошибку при вводе имен. Добавил собственно алгоритм самой игры.

--- trunk/src/cama/Game.java
+++ trunk/src/cama/Game.java
@@ -2,15 +2,58 @@ package cama;
 import java.io.*;
 class Game
 {
-    static public void main(String[]args) throws IOException
+    public void game() throws IOException
     {
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter now = new BufferedWriter(new FileWriter("now.txt"));
+        String[]nowstr ={"W W W","E E E","B B B"};
+        for(int i =0; i < 3; i++){
+            now.write(nowstr[i]+"\r\n");
+        }
+        now.close();
+        HP h1 = new HP();
+        HP h2 = new HP();
         Konsol ks = new Konsol();
-        ks.start();        
+        ks.start();       
         w.write("\r\n"+"Enter your names: ");
-        w.flush();
-        String[]s = r.readLine().split(" ");
+        w.flush();        
+        h1.b = true;
+        h2.b = false;
+        String s[] = r.readLine().split(" ");
+        if(s.length!=2){
+            while(s.length!=2){
+                w.write("Try again!\r\n");
+                w.flush();
+                s = r.readLine().split(" ");
+            }
+        }
+        if(s.length==2){
+            h1.name = s[0];
+            h2.name = s[1];
+        }
+        w.write("\r\n");
+        String online = "none";        
+        int i = 0;
+        while(!online.equals("exit")){           
+            if(!online.equals("exit")){
+                if(i == 0){
+                    ks.who(h1);                    
+                    h1.xod(true);
+                    ks.readField("now.txt");
+                    ks.check();
+                    i = 1;
+                }else if(i == 1){
+                    ks.who(h2); 
+                    h2.xod(false);
+                    ks.readField("now.txt");
+                    ks.check();
+                    i = 0;
+                }
+            }else{                
+                System.exit(1);
+            }
+        }
         w.close();
     }
 }
