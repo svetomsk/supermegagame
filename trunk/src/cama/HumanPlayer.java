f7b87e61b7d30b20c95b66934460907525947474
chukreev55 <chukreev55@69d88170-274e-2ec1-5a88-701c5e5b7dbd>
2010-11-21 06:09:59 +0000

    Все заново

---
+++ trunk/src/cama/HumanPlayer.java
@@ -0,0 +1,10 @@
+package wearegoingdown;
+class HumanPlayer_new{
+    public String name;
+    public boolean b = true;
+    public void who (){
+        if (b ==true) b=false;
+        else b =true;
+    }
+
+}
