856850559ec33d145cbca5c60685eb182feff67b
chukreev55 <chukreev55@69d88170-274e-2ec1-5a88-701c5e5b7dbd>
2011-02-21 16:21:47 +0000

    В коммит вошли следующие изменения: 1)Возврат к старой версии класса Konsol (убрал конструктор) 2)Разложил метод check() по более простым методам 3)Создал класс Texts, в котором хранятся все стоки для вывода в консоль 4)Обработал исключения при считывании информации о ходе 5)Оптимизировал класс xod() 6)Переписал метод getField() на более удобный для пользователя 7)Оптимизировал метод who() 8)Переделал поле игры. Теперь вместо W B и E появляется некое подобие шашек. Остается нерешенным вопрос о файлах kwtext.txt и now.txt. Их следует записать в память компьютера.

--- src/cama/HP.java
+++ src/cama/HP.java
@@ -1,114 +1,117 @@
 package cama;
 import java.io.*;
 class HP{
-    private BufferedWriter bw;
+    private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));;
     private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public boolean b;
     Konsol ks1 = new Konsol();
     String name, s;
     public void xod(boolean bol)throws IOException{
-        String ok = "error";
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        while(!ok.equals("yes")){
+        Boolean ok = false;
+
+        while(ok!=true){
             s = br.readLine();
             if(s.equals("exit")) System.exit(0);
+            
+            String s1[],s2[],s3[];
+            int x1,x2,y1,y2;
+            x1=x2=y1=y2=0;
+            try{
+            s1 = s.split("-");
+            s2 = s1[0].split("");
+            s3 = s1[1].split("");
+            y1 = make_y(s2[1]);
+            y2 = make_y(s3[1]);
+            x1 = make_x(s2[2]);
+            x2 = make_x(s3[2]);
+            }
+            catch(ArrayIndexOutOfBoundsException e){
+                bw.write(Texts.Er_TEXT);
+                bw.flush();
+                continue;
+            }
+            catch(NumberFormatException er){
+                bw.write(Texts.Er_TEXT);
+                bw.flush();
+                continue;
+            }
 
-            String[]s1 = s.split("-");
-            String[]s2 = s1[0].split("");
-            String[]s3 = s1[1].split("");
-
-            int y1 = make_y(s2[1]);
-            int y2 = make_y(s3[1]);
-            int x1 = make_x(s2[2]);
-            int x2 = make_x(s3[2]);
 
             ks1.readField("now.txt");
-            if(x1<3&&x2<3&&y1<3&&y2<3){
-                if(x1>=0&&x2>=0&&y1>=0&&y2>=0){
+
+            if(x1<3 && x2<3 && y1<3 && y2<3 && x1>=0 && x2>=0 && y1>=0 && y2>=0){
                     String cell = ks1.getCh(x1, y1);
                     String cellch = ks1.getCh(x2, y2);
                     if(bol == true){
-                        if(cell.equals("E")){
-                            bw.write("It is impossible to move empty cells\r\n");
+                        if(cell.equals("▫")){
+                            bw.write(Texts.EMPTY);
                             bw.flush();
-                            ok = "error";
-                        }else if(cell.equals("W")){
-                            if(cellch.equals("W")){
-                                bw.write("It is your cell!\r\n"+
-                                   "Try again!\r\n");
+                        }else if(cell.equals("o")){
+                            if(cellch.equals("o")){
+                                bw.write(Texts.YOUR_CELL);
                                 bw.flush();
-                                ok = "error";
-                            }else if(cellch.equals("B")){
+                            }else if(cellch.equals("•")){
                                 if((x2-x1==1&&y2-y1==1)||(x2-x1==1&&y1-y2==1)){
-                                    ks1.rewrite(x2, y2, "W");
-                                    ks1.rewrite(x1, y1, "E");
+                                    ks1.rewrite(x2, y2, "o");
+                                    ks1.rewrite(x1, y1, "▫");
                                     ks1.getField();
-                                    ok = "yes";
+                                    ok = true;
                                 }else{
-                                    bw.write("ERROR\r\n");
+                                    bw.write(Texts.Er_Xod);
                                     bw.flush();
-                                    ok = "error";
                                 }
-                            }else if(cellch.equals("E")){
+                            }else if(cellch.equals("▫")){
                                 if(x2-x1==1&&y2-y1==0){
-                                    ks1.rewrite(x2, y2, "W");
-                                    ks1.rewrite(x1, y1, "E");
+                                    ks1.rewrite(x2, y2, "o");
+                                    ks1.rewrite(x1, y1, "▫");
                                     ks1.getField();
-                                    ok = "yes";
+                                    ok = true;
                                 }else{
-                                    bw.write("ERROR\r\n");
+                                    bw.write(Texts.Er_Xod);
                                     bw.flush();
-                                    ok = "error";
                                 }
                             }
-                        }else if(cell.equals("B")){
-                                  bw.write("It is not your cell!\r\n"+
-                                       "Try again!\r\n");
+                        }else if(cell.equals("•")){
+                                  bw.write(Texts.NOT_YOUR_CELL);
                                    bw.flush();
-                                   ok = "error";
                         }
-                    }else if(bol == false){
-                         if(cell.equals("E")){
-                             bw.write("It is impossible to move empty cells\r\n");
+                    }else{
+                         if(cell.equals("▫")){
+                             bw.write(Texts.EMPTY);
                              bw.flush();
-                             ok = "error";
-                         }else if(cell.equals("B")){
-                             if(cellch.equals("B")){
-                                bw.write("It is your cell\r\n"+
-                                  "Try again!\r\n");
+                         }else if(cell.equals("•")){
+                             if(cellch.equals("•")){
+                                bw.write(Texts.YOUR_CELL);
                                 bw.flush();
-                                ok = "error";
-                              }else if(cellch.equals("W")){
+                              }else if(cellch.equals("o")){
                                  if((x2-x1==-1&&y2-y1==-1)||(x2-x1==-1&&y1-y2==-1)){
-                                    ks1.rewrite(x2, y2, "B");
-                                    ks1.rewrite(x1, y1, "E");
+                                    ks1.rewrite(x2, y2, "•");
+                                    ks1.rewrite(x1, y1, "▫");
                                     ks1.getField();
-                                    ok = "yes";
+                                    ok = true;
                                  }else{
-                                    bw.write("ERROR\r\n");
+                                    bw.write(Texts.Er_Xod);
                                     bw.flush();
-                                    ok = "error";
                                  }
-                              }else if(cellch.equals("E")){
+                              }else if(cellch.equals("▫")){
                                  if(x2-x1==-1&&y2-y1==0){
-                                    ks1.rewrite(x2, y2, "B");
-                                    ks1.rewrite(x1, y1, "E");
+                                    ks1.rewrite(x2, y2, "•");
+                                    ks1.rewrite(x1, y1, "▫");
                                     ks1.getField();
-                                    ok = "yes";
+                                    ok = true;
                                  }else{
-                                    bw.write("ERROR\r\n");
+                                    bw.write(Texts.Er_Xod);
                                     bw.flush();
-                                    ok = "error";
                                  }
                               }
-                          }else if(cell.equals("W")){
-                                   bw.write("Its not your cell!\r\n"+
-                                   "Try again!\r\n");
+                          }else if(cell.equals("o")){
+                                   bw.write(Texts.NOT_YOUR_CELL);
                                    bw.flush();
-                                   ok = "error";
                          }
-                    }
                 }
+            }else{
+                bw.write(Texts.OUT_OF_BOUNDS);
+                bw.flush();
             }
         }
         
--- src/cama/Konsol.java
+++ src/cama/Konsol.java
@@ -3,24 +3,24 @@ import java.io.*;
 class Konsol {
     private BufferedWriter kw;
     private BufferedReader kr;
-    private String[][] field;
+    private String[][] field = new String [3][3];
     public boolean b;
-    Konsol() throws IOException{
-        this.b=b;
-        field = new String[3][3];
-        kw = new BufferedWriter(new OutputStreamWriter(System.out));
-        kr = new BufferedReader(new FileReader("now.txt"));
-    }
     public void start()throws IOException{
         kr = new BufferedReader(new FileReader("kwtext.txt"));
-        //kw.write("Добро пожаловать в игру \"Пешки 3х3\"!\nДля того, чтобы сделать ход, выберите\nкоординаты пешки, поставьте тире\nи выберите координаты места, куда\nхотите поставить пешку. Удачи!\n");
+        kw = new BufferedWriter(new OutputStreamWriter(System.out));
+        kw.write(Texts.GREETING);
         for(int i = 0; i <5; i++){
             kw.write(kr.readLine()+"\r\n");
         }
         kw.flush();
+        kr.close();
     }
     public void getField() throws IOException{
+        kw = new BufferedWriter(new OutputStreamWriter(System.out));
+        kr  = new BufferedReader(new FileReader("now.txt"));
+        kw.write("  a b c\r\n");
         for(int i =0;i<3;i++){
+            kw.write((i+1)+" ");
             String[]k = kr.readLine().split(" ");
             for(int g = 0;g<3;g++){
                 kw.write(k[g]+" ");
@@ -31,19 +31,12 @@ class Konsol {
         }
     }
     public String getCh(int i, int g){
-    return field[i][g];
+        return field[i][g];
     }
     public void who(HP player)throws IOException{
         kw = new BufferedWriter(new OutputStreamWriter(System.out));
-        if(player.b = true){
-            kw.write(player.name + " goes: \r\n");
-            b = false;
-            kw.flush();
-        }else{
-            kw.write(player.name + " goes: \r\n");
-            b = true;
-            kw.flush();
-        }
+        kw.write(Texts.DO_STEP(player.name));
+        kw.flush();
     }
     public void readField(String fname)throws IOException{
         kr = new BufferedReader(new FileReader(fname));
@@ -53,6 +46,7 @@ class Konsol {
         }
     }
     public void rewrite(int i, int g, String s)throws IOException{
+        kw = new BufferedWriter(new FileWriter("now.txt"));
         field[i][g]  = s;
         for(int f = 0;f<3;f++){
             for(int t =0;t<3;t++){
@@ -65,123 +59,155 @@ class Konsol {
     }
     public void check(boolean cb)throws IOException{
         badpos();
+        non_chekers();
+        last_number();
+        pat(cb);
+    }
+    private int wh_q(){
+        int count =0;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(field[i][j].equals("o")) count++;
+            }
+        }
+        return count;
+    }
+    private int bl_q(){
+        int count =0;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(field[i][j].equals("•")) count++;
+            }
+        }
+        return count;
+    }
+    private void badpos (){
+        int count=0;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(!field[i][j].equals("▫")) count++;
+            }
+        }
+        if (count==2){
+            System.out.println("White win!");
+            System.exit(0);
+        }
+    }
+    private void non_chekers(){
         b = true;
-        // ПРОВЕРКА НА НАЛИЧИЕ ПЕШЕК
             for(int i = 0; i < 3; i++){                     
                 for(int g = 0; g < 3; g++){
-                    if(field[i][g].equals("W")) b = false;
+                    if(field[i][g].equals("o")) b = false;
                 }
             }
             if(b == true){
-                kw.write("Black win!");
-                kw.flush();
+                System.out.println(Texts.BlWin);
                 System.exit(0);
             }
             b = true;
             for(int i = 0; i < 3; i++){
                 for(int g = 0; g < 3; g++){
-                    if(field[i][g].equals("B")) b = false;
+                    if(field[i][g].equals("•")) b = false;
                 }
             }
             if(b == true){
-                kw.write("While win!");
-                kw.flush();
+                System.out.println(Texts.WhWin);
                 System.exit(0);                             
             }
-            //ПРОВЕРКА НА НАЛИЧИЕ ПЕШКИ В ПОСЛЕДНЕМ РЯДУ
-            b = true;                                       
+    }
+    private void last_number(){
+         b = true;                                       
             for(int i = 0 ; i < 3; i++){
-                if(field[0][i].equals("B")) b = false;                
+                if(field[0][i].equals("•")) b = false;
             }
             if(b == false){
-                kw.write("Black win!");
-                kw.flush();
+                System.out.println(Texts.BlWin);
                 System.exit(0);
             }
             b = true;
             for(int i=0; i<3; i++){
-                if(field[2][i].equals("W")) b = false;
+                if(field[2][i].equals("o")) b = false;
             }
             if(b == false){
-                kw.write("White win!");
-                kw.flush();
+                System.out.println(Texts.WhWin);
                 System.exit(0);                                 
             }
-            //ПРОВЕРКА НА ПАТ
-            b = false;
-            boolean wh[] = new boolean[6];
-            boolean bl[] = new boolean[6];
-            for(int i=0 ; i<6 ;i++){
+    }
+    private void pat(boolean cb){
+        b = false;
+            boolean wh[] = new boolean[3];
+            boolean bl[] = new boolean[3];
+            for(int i=0 ; i<3 ;i++){
                 wh[i] = true;
                 bl[i] = true;
             }
             int iw = 0;
             int ib = 0;
-            if(field[0][0].equals("W")){
-                if(!field[1][1].equals("B")&&!field[1][0].equals("E")){
+            if(field[0][0].equals("o")){
+                if(!field[1][1].equals("•")&&!field[1][0].equals("▫")){
                     wh[iw]=false;
                     iw++;
                 }
             }
-            if(field[0][1].equals("W")){
-                if(!field[1][0].equals("B")&&!field[1][1].equals("E")&&!field[1][2].equals("B")){
+            if(field[0][1].equals("o")){
+                if(!field[1][0].equals("•")&&!field[1][1].equals("▫")&&!field[1][2].equals("•")){
                     wh[iw]= false;
                     iw++;
                 }
             }
-            if(field[0][2].equals("W")){
-                if(!field[1][1].equals("B")&&!field[1][2].equals("E")){
+            if(field[0][2].equals("o")){
+                if(!field[1][1].equals("•")&&!field[1][2].equals("▫")){
                     wh[iw]=false;
                     iw++;
                 }
             }
-            if(field[1][0].equals("W")){
-                if(!field[2][0].equals("E")&&!field[2][1].equals("B")){
+            if(field[1][0].equals("o")){
+                if(!field[2][0].equals("▫")&&!field[2][1].equals("•")){
                     wh[iw]=false;
                     iw++;
                 }
-            }else if (field[1][0].equals("B")){
-                if(!field[0][0].equals("E")&&!field[0][1].equals("W")){
+            }else if (field[1][0].equals("•")){
+                if(!field[0][0].equals("▫")&&!field[0][1].equals("o")){
                     bl[ib] = false;
                     ib++;
                 }
             }
-            if(field[1][1].equals("W")){
-                if(!field[2][0].equals("B")&&!field[2][2].equals("B")&&!field[2][1].equals("E")){
+            if(field[1][1].equals("o")){
+                if(!field[2][0].equals("•")&&!field[2][2].equals("•")&&!field[2][1].equals("▫")){
                     wh[iw]=false;
                     iw++;
                 }
-            }else if(field[1][1].equals("B")){
-                if(!field[0][0].equals("W")&&!field[0][2].equals("W")&&!field[0][1].equals("E")){
+            }else if(field[1][1].equals("•")){
+                if(!field[0][0].equals("o")&&!field[0][2].equals("o")&&!field[0][1].equals("▫")){
                     bl[ib] = false;
                     ib++;
                 }
             }
-            if(field[1][2].equals("W")){
-                if(!field[2][2].equals("E")&&!field[2][1].equals("B")){
+            if(field[1][2].equals("o")){
+                if(!field[2][2].equals("▫")&&!field[2][1].equals("•")){
                     wh[iw]=false;
                     iw++;
                 }
-            }else if(field[1][2].equals("B")){
-                if(!field[0][2].equals("E")&&!field[0][1].equals("W")){
+            }else if(field[1][2].equals("•")){
+                if(!field[0][2].equals("▫")&&!field[0][1].equals("o")){
                     bl[ib] = false;
                     ib++;
                 }
             }
-            if(field[2][0].equals("B")){
-                if(!field[1][0].equals("E")&&!field[1][1].equals("W")){
+            if(field[2][0].equals("•")){
+                if(!field[1][0].equals("▫")&&!field[1][1].equals("o")){
                     bl[ib] = false;
                     ib++;
                 }
             }
-            if(field[2][1].equals("B")){
-                if(!field[1][0].equals("W")&&!field[1][2].equals("W")&&field[1][1].equals("E")){
+            if(field[2][1].equals("•")){
+                if(!field[1][0].equals("o")&&!field[1][2].equals("o")&&field[1][1].equals("▫")){
                     bl[ib] = false;
                     ib++;
                 }
             }
-            if(field[2][2].equals("B")){
-                if(!field[1][1].equals("W")&&!field[1][2].equals("E")){
+            if(field[2][2].equals("•")){
+                if(!field[1][1].equals("o")&&!field[1][2].equals("▫")){
                     bl[ib] = false;
                     ib++;
                 }
@@ -194,8 +220,7 @@ class Konsol {
                     }
                 }
                 if(b == false){
-                    kw.write("Black win!");
-                    kw.flush();
+                    System.out.println(Texts.BlWin);
                     System.exit(0);
                 }
             }else{
@@ -206,42 +231,9 @@ class Konsol {
                     }
                 }
                 if(b == false){
-                    kw.write("White win!");
-                    kw.flush();
+                    System.out.println(Texts.WhWin);
                     System.exit(0);
                 }
             }
-
-
-    }
-    private int wh_q(){
-        int count =0;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                if(field[i][j].equals("W")) count++;
-            }
-        }
-        return count;
-    }
-    private int bl_q(){
-        int count =0;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                if(field[i][j].equals("B")) count++;
-            }
-        }
-        return count;
-    }
-    private void badpos (){
-        int count=0;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                if(!field[i][j].equals("E")) count++;
-            }
-        }
-        if (count==2){
-            System.out.println("White win!");
-            System.exit(0);
-        }
     }
 }
