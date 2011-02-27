c1907420bec24458642549dbacb59b8990a05d91
chukreev55 <chukreev55@69d88170-274e-2ec1-5a88-701c5e5b7dbd>
2011-02-27 06:18:58 +0000

    Добавили в проект классы Model IPlayer Player

---
+++ src/cama/IPlayer.java
@@ -0,0 +1,6 @@
+package cama;
+interface IPlayer {
+    public void xod(boolean isWhite);
+    public String getName();
+    public void setName(String value);
+}
---
+++ src/cama/Player.java
@@ -0,0 +1,10 @@
+package cama;
+abstract class Player implements IPlayer {
+private String name;
+ public String getName(){
+        return name;
+    }
+ public void setName(String value){
+        name = value;
+    }
+}
