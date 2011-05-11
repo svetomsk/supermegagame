f70ed75e13a6a935a8e6e0970d480a231f5cc7a4
chukreev55 <chukreev55@69d88170-274e-2ec1-5a88-701c5e5b7dbd>
2011-05-11 14:13:26 +0000

    Создал класс Step. Теперь все ходы проходят через него, и, как следствие, убрал rewrite(). Осталось только переделать GHPlayer.

--- src/cama/console_cama/HPlayer.java
+++ src/cama/console_cama/HPlayer.java
@@ -3,229 +3,107 @@ import java.io.*;
 import cama.core.Player;
 import cama.core.Texts;
 import cama.core.Judge;
+import cama.core.Step;
 
 public class HPlayer extends Player
 {
     private BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));;
     private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public boolean b;
-    private Judge ks;
+    private Judge judge;
 
     public HPlayer(Judge ks_from_main) {
-        ks = ks_from_main;
+        judge = ks_from_main;
     }
-    
-    public void doStep(boolean isWhite){
-        Boolean ok = false;
-        String s = "";
 
-        while(ok!=true){
-            try{
-            s = br.readLine();
-            }
-            catch(IOException one){
-                System.out.println(Texts.IOException);
-                System.exit(0);
-            }
-            if(s.equals("exit")) System.exit(0);
-            if(s.equals("GK")){
-                System.out.println("Texts.CHEAT1");
-                System.exit(0);
-            }
+    public void doStep(boolean isWhite) {
+        Step step = null;
+        try {
+            step = getStepFromKeyboard(isWhite);
+        } catch (IOException e) {
+            System.out.println("InputOutputException!! TOTAL FAIL!!!");
+            System.exit(1);
+        }
+
+        judge.handleStep(step, isWhite);
+    }
+    private Step getStepFromKeyboard(boolean isWhite) throws IOException{
+        Step st = null;
+        while (st == null){
+          String str = br.readLine();
+          st = parseStr(str);
 
 
-            String s1[],s2[],s3[];
-            int x1,x2,y1,y2;
-            x1=x2=y1=y2=0;
-            try{
+          if(st == null){
+              bw.write(Texts.ER_TEXT);
+              bw.flush();
+              continue;
+          }
+
+          if(!judge.isStepCorrect(st, isWhite)){
+              System.out.println("st.col1 "+st.col1);
+              System.out.println("st.col2 "+st.col2);
+              System.out.println("st.row1 "+st.row1);
+              System.out.println("st.row2 "+st.row2);
+              st = null;
+                      System.out.println("here!!");
+              bw.write(Texts.ER_STEP);
+              bw.flush();
+          }
+       }
+       return st;
+    }
+    private Step parseStr(String s){
+        Step st = null;
+
+        if(s.equals("exit")){
+            System.exit(0);
+        }
+
+        if (s.equals("GK")) {
+            System.out.println("Texts.CHEAT1");
+            System.exit(0);
+        }
+
+
+        String s1[], s2[], s3[];
+        int x1, x2, y1, y2;
+        x1 = x2 = y1 = y2 = 0;
+        try {
             s1 = s.split("-");
             s2 = s1[0].split("");
             s3 = s1[1].split("");
-            y1 = makeY(s2[1]);
-            y2 = makeY(s3[1]);
-            x1 = makeX(s2[2]);
-            x2 = makeX(s3[2]);
-            }
-            catch(ArrayIndexOutOfBoundsException e){
-                try{
-                    bw.write(Texts.ER_TEXT);
-                    bw.flush();
-                    continue;
-                }
-                catch(IOException one){
-                    System.out.println(Texts.IOException);
-                    System.exit(0);
-                }
-            }
-            catch(NumberFormatException er){
-                try{
-                    bw.write(Texts.ER_TEXT);
-                    bw.flush();
-                    continue;
-                }
-                catch(IOException one){
-                    System.out.println(Texts.IOException);
-                    System.exit(0);
-                }
-            }
-
-            if(x1<ks.getSize() && x2<ks.getSize() && y1<ks.getSize() && y2<ks.getSize()
-               && x1>=0 && x2>=0 && y1>=0 && y2>=0){
-                    int cell = ks.getCh(x1, y1);
-                    int cellch = ks.getCh(x2, y2);
-                    if(isWhite == true){
-                        if(cell==ks.E){
-                            try{
-                            bw.write(Texts.EMPTY_LN);
-                            bw.flush();
-                            }
-                            catch(IOException one){
-                                System.out.println(Texts.IOException);
-                                System.exit(0);
-                            }
-                        }else if(cell==ks.W){
-                            if(cellch==ks.W){
-                                try{
-                                    bw.write(Texts.YOUR_CELL);
-                                    bw.flush();
-                                }
-                                catch(IOException one){
-                                    System.out.println(Texts.IOException);
-                                    System.exit(0);
-                                }
-                            }else if(cellch==ks.B){
-                                if((x2-x1==1&&y2-y1==1)||(x2-x1==1&&y1-y2==1)){
-                                    ks.rewrite(x2, y2, ks.W);
-                                    ks.rewrite(x1, y1, ks.E);
-                                    //ks.printField();
-                                    ok = true;
-                                }else{
-                                    try{
-                                        bw.write(Texts.Er_Xod);
-                                        bw.flush();
-                                    }
-                                    catch(IOException one){
-                                        System.out.println(Texts.IOException);
-                                        System.exit(0);
-                                    }
-                                }
-                            }else if(cellch==ks.E){
-                                if(x2-x1==1&&y2-y1==0){
-                                    ks.rewrite(x2, y2, ks.W);
-                                    ks.rewrite(x1, y1, ks.E);
-                                    //ks.printField();
-                                    ok = true;
-                                }else{
-                                    try{
-                                        bw.write(Texts.Er_Xod);
-                                        bw.flush();
-                                    }
-                                    catch(IOException one){
-                                        System.out.println(Texts.IOException);
-                                        System.exit(0);
-                                    }
-
-                                }
-                            }
-                        }else if(cell==ks.B){
-                            try{
-                                bw.write(Texts.NOT_YOUR_CELL_LN);
-                                bw.flush();
-                            }
-                            catch(IOException one){
-                                System.out.println(Texts.IOException);
-                                System.exit(0);
-                            }
-                        }
-                    }else{
-                         if(cell==ks.E){
-                             try{
-                                bw.write(Texts.EMPTY_LN);
-                                bw.flush();
-                             }
-                             catch(IOException one){
-                                System.out.println(Texts.IOException);
-                                System.exit(0);
-                            }
-                         }else if(cell==ks.B){
-                             if(cellch==ks.B){
-                                 try{
-                                    bw.write(Texts.YOUR_CELL);
-                                    bw.flush();
-                                 }
-                                 catch(IOException one){
-                                    System.out.println(Texts.IOException);
-                                    System.exit(0);
-                                 }
-                              }else if(cellch==ks.W){
-                                 if((x2-x1==-1&&y2-y1==-1)||(x2-x1==-1&&y1-y2==-1)){
-                                    ks.rewrite(x2, y2, ks.B);
-                                    ks.rewrite(x1, y1, ks.E);
-                                    //ks.printField();
-                                    ok = true;
-                                 }else{
-                                    try{
-                                        bw.write(Texts.Er_Xod);
-                                        bw.flush();
-                                    }
-                                    catch(IOException one){
-                                        System.out.println(Texts.IOException);
-                                        System.exit(0);
-                                    }
-                                 }
-                              }else if(cellch==ks.E){
-                                 if(x2-x1==-1&&y2-y1==0){
-                                    ks.rewrite(x2, y2, ks.B);
-                                    ks.rewrite(x1, y1, ks.E);
-                                    //ks.printField();
-                                    ok = true;
-                                 }else{
-                                    try{
-                                    bw.write(Texts.Er_Xod);
-                                    bw.flush();
-                                    }
-                                    catch(IOException one){
-                                        System.out.println(Texts.IOException);
-                                        System.exit(0);
-                                    }
-                                 }
-                              }
-                          }else if(cell==ks.W){
-                              try{
-                                   bw.write(Texts.NOT_YOUR_CELL_LN);
-                                   bw.flush();
-                              }
-                              catch(IOException one){
-                                System.out.println(Texts.IOException);
-                                System.exit(0);
-                              }
-                         }
-                }
-            }else{
-                try{
-                    bw.write(Texts.OUT_OF_BOUNDS);
-                    bw.flush();
-                }
-                catch(IOException one){
-                    System.out.println(Texts.IOException);
-                    System.exit(0);
-                }
+            y1 = makeX(s2[1]);
+            y2 = makeX(s3[1]);
+            x1 = makeY(s2[2]);
+            x2 = makeY(s3[2]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            try {
+                bw.write(Texts.ER_TEXT);
+                bw.flush();
+                return null;
+            } catch (IOException one) {
+                System.out.println(Texts.IOException);
+                System.exit(0);
             }
         }
-        
-    }  
-    private int makeY(String m){
+
+        st = new Step(y1, x1, y2, x2);
+        return st;
+    }
+
+    private int makeX(String m){
         int y=0;
         if(m.equals("a"))     y = 0;
         if(m.equals("b"))     y = 1;
         if(m.equals("c"))     y = 2;
         return y;
     }
-    private int makeX(String m){
+    private int makeY(String m){
         return Integer.valueOf(m)-1;
     }
     public void finish() throws IOException{
         bw.close();
         br.close();
-    } // Надо привязать!!!
+    }
 }
---
+++ src/cama/core/Step.java
@@ -0,0 +1,13 @@
+package cama.core;
+public class Step {
+    public int col1;
+    public int row1;
+    public int col2;
+    public int row2;
+    public Step(int col1, int row1, int col2, int row2){
+        this.col1 = col1;
+        this.col2 = col2;
+        this.row1 = row1;
+        this.row2 = row2;
+    }
+}
