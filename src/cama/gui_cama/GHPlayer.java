cd79035c141e20423c5772cbf901c0df41cca59e
chukreev55 <chukreev55@69d88170-274e-2ec1-5a88-701c5e5b7dbd>
2011-05-15 03:46:31 +0000

    Починили класс GHPlayer. Надо будет проверить, когда полностью починится графическая версия

--- src/cama/gui_cama/GHPlayer.java
+++ src/cama/gui_cama/GHPlayer.java
@@ -3,13 +3,15 @@ package cama.gui_cama;
 import cama.core.Judge;
 import cama.core.Player;
 import cama.core.Texts;
+import cama.core.Step;
 
 class GHPlayer extends Player {
 
     public boolean b;
-    private Judge ks;
+    private Judge judge;
+    private Step step;
     GHPlayer(Judge ks_from_main) {
-        ks = ks_from_main;
+        judge = ks_from_main;
 		setModulePlayer(false);
    }
     public void doStep(boolean isWhite) {
@@ -19,15 +21,6 @@ class GHPlayer extends Player {
         }*/
 
         String s = Main.coord.getText();
-//        String[] s1 = Main.s1.split("");
-//        String[] s2 = Main.s2.split("");
-
-//        int x1 = Integer.valueOf(s1[1]);
-//        int y1 = Integer.valueOf(s1[2]);
-//        int x2 = Integer.valueOf(s2[1]);
-//        int y2 = Integer.valueOf(s2[2]);
-
-
 
         int x1, x2, y1, y2;
         String[] s1, s2, s3;
@@ -36,78 +29,77 @@ class GHPlayer extends Player {
             s1 = s.split("-");
             s2 = s1[0].split("");
             s3 = s1[1].split("");
-            y1 = makeY(s2[1]);
-            y2 = makeY(s3[1]);
-            x1 = makeX(s2[2]);
-            x2 = makeX(s3[2]);
+            y1 = makeX(s2[1]);
+            y2 = makeX(s3[1]);
+            x1 = makeY(s2[2]);
+            x2 = makeY(s3[2]);
         } catch (ArrayIndexOutOfBoundsException e) {
             Main.ErLab1.setText(Texts.ER_TEXT);
         } catch (NumberFormatException er) {
             Main.ErLab1.setText(Texts.ER_TEXT);
         }
 
-
-
-        //if (x1 < ks.getSize() && x2 < ks.getSize() && y1 < ks.getSize() && y2 < ks.getSize()
-        //        && x1 >= 0 && x2 >= 0 && y1 >= 0 && y2 >= 0) {
-
-            int cell = ks.getCh(x1, y1);
-            int cellch = ks.getCh(x2, y2);
+            int cell = judge.getCh(x1, y1);
+            int cellch = judge.getCh(x2, y2);
             if(isWhite){
-                if(cell == ks.W && cellch ==ks.B){
+                if(cell == judge.W && cellch ==judge.B){
                     if ((x2 - x1 == 1 && y2 - y1 == 1) || (x2 - x1 == 1 && y1 - y2 == 1)) {
-                        ks.rewrite(x2, y2, ks.W);
-                        ks.rewrite(x1, y1, ks.E);
                         setIsWhite();
                         Main.ErLab1.setText("");
+                        //judge.rewrite(x2, y2, judge.W);
+                        //judge.rewrite(x1, y1, judge.E);
+                        makeStep(y1, x1, y2, x2, !isWhite);
                     }else{
                         System.out.println("here 1");
-                        Main.ErLab1.setText(Texts.ER_STEP);
+                        Main.ErLab1.setText(Texts.ER_STEP_LN);
                     }
-                }else if(cell == ks.W && cellch ==ks.E){
+                }else if(cell == judge.W && cellch ==judge.E){
                     if (x2 - x1 == 1 && y2 - y1 == 0) {
-                            ks.rewrite(x2, y2, ks.W);
-                            ks.rewrite(x1, y1, ks.E);
                             setIsWhite();
                             Main.ErLab1.setText("");
+                            //judge.rewrite(x2, y2, judge.W);
+                            //judge.rewrite(x1, y1, judge.E);
+                            makeStep(y1, x1, y2, x2, !isWhite);
                     }else{
                         System.out.println("here 2");
-                        Main.ErLab1.setText(Texts.ER_STEP);
+                        Main.ErLab1.setText(Texts.ER_STEP_LN);
                     }
                 }else{
                     System.out.println("here 3");
                     System.out.println(cell+" "+cellch);
-                    Main.ErLab1.setText(Texts.ER_STEP);
+                    Main.ErLab1.setText(Texts.ER_STEP_LN);
                 }
             }else{
-                if(cell == ks.B && cellch == ks.W){
+                if(cell == judge.B && cellch == judge.W){
                     if ((x2 - x1 == -1 && y2 - y1 == -1) || (x2 - x1 == -1 && y1 - y2 == -1)) {
-                        ks.rewrite(x2, y2, ks.B);
-                        ks.rewrite(x1, y1, ks.E);
                         setIsWhite();
                         Main.ErLab1.setText("");
+                        //judge.rewrite(x2, y2, judge.B);
+                        //judge.rewrite(x1, y1, judge.E);
+                        makeStep(y1, x1, y2, x2, !isWhite);
                     } else {
                         System.out.println("here 4");
-                          Main.ErLab1.setText(Texts.ER_STEP);
+                          Main.ErLab1.setText(Texts.ER_STEP_LN);
                     }
-                }else if(cell == ks.B && cellch == ks.E){
+                }else if(cell == judge.B && cellch == judge.E){
                     if (x2 - x1 == -1 && y2 - y1 == 0) {
-                        ks.rewrite(x2, y2, ks.B);
-                        ks.rewrite(x1, y1, ks.E);
                         setIsWhite();
                         Main.ErLab1.setText("");
+                        //judge.rewrite(x2, y2, judge.B);
+                        //judge.rewrite(x1, y1, judge.E);
+                        makeStep(y1, x1, y2, x2, !isWhite);
                     } else {
                         System.out.println("here 5");
-                        Main.ErLab1.setText(Texts.ER_STEP);
+                        Main.ErLab1.setText(Texts.ER_STEP_LN);
                     }
                 } else {
                     System.out.println("here 6");
-                    Main.ErLab1.setText(Texts.ER_STEP);
+                    Main.ErLab1.setText(Texts.ER_STEP_LN);
                 }
             }
     }
 
-    private int makeY(String m) {
+    private int makeX(String m) {
         int y = 0;
         if (m.equals("a")) {
             y = 0;
@@ -120,7 +112,7 @@ class GHPlayer extends Player {
         }
         return y;
     }
-    private int makeX(String m) {
+    private int makeY(String m) {
         return Integer.valueOf(m) - 1;
     }
     private void setIsWhite(){
@@ -129,5 +121,9 @@ class GHPlayer extends Player {
         }else{
             Main.isWhite = true;
         }
+    }
+    private void makeStep(int col1, int row1, int col2, int row2, boolean isWhite){
+        step = new Step(col1, row1, col2, row2);
+        judge.handleStep(step, isWhite);
     }
 }
