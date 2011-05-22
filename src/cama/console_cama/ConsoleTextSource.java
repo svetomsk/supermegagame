952798dda66411792544c7cd13107fd295a308b9
chukreev55 <chukreev55@69d88170-274e-2ec1-5a88-701c5e5b7dbd>
2011-05-22 02:36:18 +0000

    Начал делать задачу

---
+++ src/cama/console_cama/ConsoleTextSource.java
@@ -0,0 +1,14 @@
+package cama.console_cama;
+
+import cama.core.ITextSource;
+import java.io.*;
+
+public class ConsoleTextSource implements ITextSource {
+
+    public ConsoleTextSource(BufferedReader br, BufferedWriter bw){
+    }
+
+    public String getStepText() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
